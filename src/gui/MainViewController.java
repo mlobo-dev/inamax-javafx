a45c8ccdf50768181d8d8c2fb89bdaaf8c0ed474
mlobo-dev <mlobo.dev@gmail.com>
2019-11-09 11:54:19 -0300

    MainView Controller

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,33 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+
+	@FXML
+	private MenuItem menuItemNew;
+	
+	@FXML 
+	private MenuItem menuItemAbout;
+	
+	@FXML
+	private void onMenuItemNewAction() {
+		System.out.println("Menu item New");
+	}
+	
+	@FXML
+	private void onMenuItemAboutAction() {
+		System.out.println("Menu item About");
+	}
+
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		
+	}
+}
